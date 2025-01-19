c7dc734c9269e4ff7b9d4c7cd0108ddb4870c0d6
Jadie <koola976@gmail.com>
2025-01-19 15:13:32 +0900

    test(failed) : statement 테스트 추가

--- src/test/java/chapter01/StatementTest.java
+++ src/test/java/chapter01/StatementTest.java
@@ -30,4 +30,22 @@ public class StatementTest {
         playMap = objectMapper.readValue(playsFile, new TypeReference<Map<String, Play>>() {});
         invoices = objectMapper.readValue(invoicesFile, new TypeReference<List<Invoices>>() {});
     }
+
+    @Test
+    void statement() {
+        Statement statement = new Statement();
+
+        String result = statement.statement();
+
+        assertThat(result).isEqualTo(
+                """
+                        청구 내역 (고객명: BigCo)
+                        Hamlet: $650.00 (55석)
+                        As You Like It: $580.00 (35석)
+                        Othello: $500.00 (40석)
+                        총액: $1,730.00
+                        적립 포인트: 47점
+                        """
+        );
+    }
 }
