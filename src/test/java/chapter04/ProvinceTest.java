ed89f48cf0e5464e3eebad4d08ed8fdb7d91d5d4
Jadie <koola976@gmail.com>
2025-02-18 23:38:03 +0900

    refactor: 4.4장 beforeEach 구현

--- src/test/java/chapter04/ProvinceTest.java
+++ src/test/java/chapter04/ProvinceTest.java
@@ -1,5 +1,6 @@
 package chapter04;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,29 +10,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProvinceTest {
 
-    @DisplayName("Province 생성 테스트")
-    @Test
-    void make() {
+    private Province province;
+
+    @BeforeEach
+    void setUp() {
         List<Producer> producers = List.of(
                 new Producer("Byzantium", 10, 9),
                 new Producer("Byzantium", 12, 12),
                 new Producer("Byzantium", 10, 6)
         );
-        Province province = new Province("Asia", 30, 20, producers);
+        province = new Province("Asia", 30, 20, producers);
+    }
 
+    @DisplayName("Province 생성 테스트")
+    @Test
+    void make() {
         assertThat(province).isNotNull();
     }
 
     @DisplayName("생산 부족분 계산")
     @Test
     void calculateShortfallTest() {
-        List<Producer> producers = List.of(
-                new Producer("Byzantium", 10, 9),
-                new Producer("Byzantium", 12, 12),
-                new Producer("Byzantium", 10, 6)
-        );
-        Province province = new Province("Asia", 30, 20, producers);
-
         assertThat(province.shortfall()).isEqualTo(30);
     }
 }
