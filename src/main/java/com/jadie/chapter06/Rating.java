ef63ea9360cc71d211b5e9469a593d5f91417337
Jadie <koola976@gmail.com>
2025-03-02 13:03:01 +0900

    feat: 6.2 리팩터링 후

--- src/main/java/com/jadie/chapter06/Rating.java
+++ src/main/java/com/jadie/chapter06/Rating.java
@@ -3,7 +3,7 @@ package com.jadie.chapter06;
 public class Rating {
 
     public int getRating(Driver driver) {
-        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+        return driver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
     }
 
     public boolean moreThanFiveLateDeliveries(Driver driver) {
