fdeef231a64dcb89c1b28dd7cd53479c59bef30b
Jadie <koola976@gmail.com>
2025-03-02 14:26:14 +0900

    feat: 6.5 리팩터링 후

--- src/main/java/com/jadie/chapter06/Circumference.java
+++ src/main/java/com/jadie/chapter06/Circumference.java
@@ -3,7 +3,7 @@ package com.jadie.chapter06;
 public class Circumference {
 
 
-    public double circum(Integer radius) {
+    public double circumference(Integer radius) {
         return 2 * Math.PI * radius;
     }
 }
