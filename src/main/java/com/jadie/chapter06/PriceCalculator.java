2d5c78fac7a5b8c804a4affc553521f3167f8f09
Jadie <koola976@gmail.com>
2025-03-02 13:16:03 +0900

    feat: 6.3 리팩터링 후

--- src/main/java/com/jadie/chapter06/PriceCalculator.java
+++ src/main/java/com/jadie/chapter06/PriceCalculator.java
@@ -3,8 +3,9 @@ package com.jadie.chapter06;
 public class PriceCalculator {
 
     public double calculate(Order order) {
-        return order.getQuantity() * order.getItemPrice() -
-                Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05 +
-                Math.min(order.getQuantity() * order.getItemPrice() * 0.1, 100);
+        int basePrice = order.getQuantity() * order.getItemPrice();
+        double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+        double shipping = Math.min(order.getQuantity() * order.getItemPrice() * 0.1, 100);
+        return basePrice - quantityDiscount + shipping;
     }
 }
