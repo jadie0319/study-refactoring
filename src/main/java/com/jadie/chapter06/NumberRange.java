369cc7148af5e1211bd75e0ec59c5255dbb09741
Jadie <koola976@gmail.com>
2025-03-03 11:33:36 +0900

    feat: 6.8 묶은 데이터를 표현할 클래스 선언

---
+++ src/main/java/com/jadie/chapter06/NumberRange.java
@@ -0,0 +1,19 @@
+package com.jadie.chapter06;
+
+public class NumberRange {
+    private Integer min;
+    private Integer max;
+
+    public NumberRange(Integer min, Integer max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public Integer min() {
+        return min;
+    }
+
+    public Integer max() {
+        return max;
+    }
+}
