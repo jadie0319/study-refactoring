43252b538aad13619406146d4707020b90a2549c
Jadie <koola976@gmail.com>
2025-03-03 14:55:42 +0900

    feat: 7.4 리팩터링 후

--- src/main/java/com/jadie/chapter07/Order.java
+++ src/main/java/com/jadie/chapter07/Order.java
@@ -10,11 +10,23 @@ public class Order {
     }
 
     public double calculate() {
-        int basePrice = this.quantity * this.item;
-        if (basePrice > 1000) {
-            return basePrice * 0.95;
+        if (basePrice() > 1000) {
+            return basePrice() * 0.95;
         } else {
-            return basePrice * 0.98;
+            return basePrice() * 0.98;
         }
+    }
+
+    public double price() {
+        return basePrice() * discountFactor();
+    }
+
+    public Integer basePrice() {return quantity * item;}
+    public double discountFactor() {
+        double discountFactor = 0.98;
+        if (basePrice() > 1000) {
+            discountFactor -= 0.03;
+        }
+        return discountFactor;
     }
 }
