81022fd6a06ca59795ff8797a1140094f4200b18
Jadie <koola976@gmail.com>
2025-03-03 12:55:22 +0900

    feat: 6.11 리팩터링 끝

--- src/main/java/com/jadie/chapter06/PriceCalculator.java
+++ src/main/java/com/jadie/chapter06/PriceCalculator.java
@@ -2,22 +2,29 @@ package com.jadie.chapter06;
 
 public class PriceCalculator {
 
-    public Integer priceOrder(Product product, Integer quantity, ShippingMethod shippingMethod) {
+    public Integer priceOrder(
+            Product product, Integer quantity,
+            ShippingMethod shippingMethod)
+    {
+        PriceData priceData = calculatePricingData(product, quantity);
+        return applyShipping(priceData,shippingMethod);
+    }
+
+    public PriceData calculatePricingData(Product product, Integer quantity) {
         int basePrice = product.basePrice() * quantity;
         int discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        Integer price = applyShipping(basePrice,shippingMethod,quantity,discount);
-        return price;
+        return new PriceData(basePrice, quantity, discount);
     }
 
     public Integer applyShipping(
-            Integer basePrice, ShippingMethod shippingMethod,
-            Integer quantity, Integer discount) {
-        Integer shippingPerCase = (basePrice > shippingMethod.discountThreshold())
+            PriceData priceData, ShippingMethod shippingMethod)
+    {
+        Integer shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold())
                 ? shippingMethod.discountedFee()
                 : shippingMethod.feePerCase();
-        Integer shippingCost = quantity * shippingPerCase;
-        return basePrice - discount + shippingCost;
+        Integer shippingCost = priceData.quantity() * shippingPerCase;
+        return priceData.basePrice() - priceData.discount() + shippingCost;
     }
 
 }
---
+++ src/main/java/com/jadie/chapter06/PriceData.java
@@ -0,0 +1,7 @@
+package com.jadie.chapter06;
+
+public record PriceData(
+        Integer basePrice,
+        Integer quantity,
+        int discount) {
+}
